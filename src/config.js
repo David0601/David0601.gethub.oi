628173d57e54a910b5f5a38b17489860184cff3f
Yuanyan <2407779683xyy@gmail.com>
2023-03-31 09:44:55 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,127 +1,121 @@
 export default {
-  // page1部分
+  // page1
   page1: {
-    titleEn: 'Hello,I`m WeiTingting', //英文标题
-    title: '一个追梦在前端路上的小白', //中文标题
+    titleEn: 'Hello,I`m Yuanyan Xu', 
+    title: 'One student just graduated from Assiniboine Community College.', 
   },
-  // page2部分
+  // page2
   page2: {
-    authorImg: 'page1.jpg', // 作者头像
-    xinhui: '我叫卫婷婷，是一名在读的大三学生。', // 幸会
-    qiuzhi: '前端开发', // 求职意向
-    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+    authorImg: 'page1.jpg', 
+    xinhui: 'My name is Yuanyan Xu.', 
+    qiuzhi: 'FRONT-END', 
+    guanyuwo: 'Know front-end knowledge, html, css basic technology, UI framework, nodejs, etc., have practical experience in college projects. Hope to learn and make progress together with you!' 
   },
-  // page3部分
+  // page3
   page3: [{
     icon: 'icon-tubiao-',
-    title: 'UI设计',
-    msg: ['PS切图、界面排版']
+    title: 'UI Design Adobe Creative',
+    msg: ['PS,AI,PR,XD']
   },{
-    icon: 'icon-diannao', // 图标
-    title: '网页制作', // 标题
-    msg: ['响应式页面', 'css、scss动画效果',] //介绍
+    icon: 'icon-diannao', 
+    title: 'Website Design', 
+    msg: ['Responsive page', 'css animation effect',] 
   }, {
     icon: 'icon-qianbi1',
-    title: '前端功能',
-    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
+    title: 'front-end function',
+    msg: ['HTML CSS JS NODEJS PHP']
   },  {
     icon: 'icon-shouji',
-    title: '框架',
-    msg: ['vue框架','UI框架、express框架、mysql',]
+    title: 'framework',
+    msg: ['PYTHON FRAMEWORK','UI FRAMEWORK 、NODEJS 、mysql',]
   }],
-  // page4部分
+  // page4
   page4: {
-    // 我的历程
+    
     course: [{
-      date: '2020/9——至今', // 时间
-      desc: { // 经历
-        title: '山西传媒学院',
-        list: ['山西省职业技能大赛web前端开发一等奖', '专升本上岸', '软件开发公司实习']
+      date: '09/2021-06/2023', 
+      desc: { 
+        title: 'Assiniboine Community College',
+        list: ['Web and Interactive Development', 'Skills Manitoba', 
+          'Front-end Development', 'Back-end Development']
       }
     }, {
-      date: '2019/9——2020/9',
+      date: '05/2017-08/2019',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习前端语言', '构建页面 / 利用JS、CSS3等制作网站，vue有关项目', '维护508工作室','获得Web前段开发1+X证书','普通话二级甲等','国家励志奖学金']
-      }
-    }, {
-      date: '2018/9——2019/9',
-      desc: {
-        title: '晋城职业技术学院',
-        list: ['学习java语言', '利用Android、sql等制作app', '参加山西省职业院校移动互联技能大赛', '全国计算机等级二级证书', '国家励志奖学金']
+        title: 'Red River College ',
+        list: ['Culinary Art skill']
       }
     }],
-    // 我的拓展技能掌握
+
     singlelist: [{
-      title: 'bootsrap',
-      text: '了解'
+      title: 'Bootstrap',
+      text: 'Be familiar with'
     }, {
-      title: 'Element-ui',
-      text: '掌握'
+      title: '',
+      text: 'Understand'
     }, {
-      title: 'mint-ui',
-      text: '掌握'
+      title: 'Adobe Creative',
+      text: 'Be familiar with'
     }, {
-      title: 'scss',
-      text: '掌握'
+      title: 'Django',
+      text: 'Be familiar with'
     }],
-    // 我的基本技能掌握
+    //Basic skills
     proresslist: [{
-      title: 'HTML、CSS、SCSS',
-      value: '90%'
+      title: 'HTML、CSS',
+      value: '60%'
     }, {
       title: 'JS、JQ',
-      value: '90%'
+      value: '40%'
     }, {
-      title: 'Vue',
-      value: '90%'
+      title: 'Python',
+      value: '40%'
     }, {
       title: 'NodeJs',
-      value: '80%'
+      value: '40%'
     }, {
-      title: 'sql',
-      value: '80%'
+      title: 'MYSQL',
+      value: '40%'
     }]
   },
   // page5部分
   page5: [{
-    title: '508工作室',
-    content: '晋城职业技术学院508工作室独立站点的维护',
+    title: '508 Studio',
+    content: '',
     image: 'box1.png',
-    href: 'https://lab508.gitee.io/'
+    href: ''
   }, {
-    title: '图书管理系统',
-    content: 'Vue,NodeJs图书管理系统前后端',
+    title: '',
+    content: '',
     image: 'box3.jpg',
-    href: 'https://gitee.com/wttAndroid/book_admin'
+    href: ''
   }, {
-    title: '校园约吧',
-    content: 'Vue,NodeJs移动端校园项目前后端',
+    title: '',
+    content: '',
     image: 'box5.jpg',
-    href: 'https://gitee.com/wttAndroid/xyy_server'
+    href: ''
   }, {
-    title: '蘑菇街',
-    content: 'Vue蘑菇街商城案例前端',
-    image: 'box2.png',
-    href: 'https://github.com/wttAndroid/MyShoppingStreet'
+    title: '',
+    content: '',
+    image: '',
+    href: ''
   }, {
-    title: '个人简历',
-    content: 'html,css,jq,bootsrap搭建个人简历网站',
+    title: 'Portfolio | Yuanyan Xu',
+    content: 'Build the Website with html,css,jq,bootstrap',
     image: 'box4.png',
     href: ''
   }, {
-    title: '二维码生成器',
-    content: 'Android二维码生成器',
+    title: 'Candle Shopping Website',
+    content: 'PHP Website',
     image: 'box6.jpeg',
     href: ''
   }],
-  // page6部分
+  // page6
   page6: {
-    github: 'https://github.com/wttAndroid',
+    github: 'https://github.com/David0601',
     bili: '',
-    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    email: 'Mailto:2407779683xyy@gmail.com?Subject=title&Body=Content',
     zhihu: '',
-    weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
-    qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
+
   }
 }
